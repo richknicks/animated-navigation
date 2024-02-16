9edc1cbac98797fb5c901ee7be1c31173eb44c9c
Richard Lynch <rlynch2012@gmail.com>
2024-02-16 00:23:38 -0500

    add toggle for menu bar and navigation animation

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// Test
+const menuBars = document.getElementById('menu-bars');
+const overLay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
+
+function toggleNav () {
+    // Toggle Menu Bars Open/Closed
+    menuBars.classList.toggle('change');
+    // Toggle: Menu Active
+    overLay.classList.toggle('overlay-active');
+    if (overLay.classList.contains('overlay-active')) {
+        // Animate In - Overlay
+        overLay.classList.remove('overlay-slide-left');
+        overLay.classList.add('overlay-slide-right');
+    } else {
+        //  Animate Out - Overlay
+        overLay.classList.remove('overlay-slide-right');
+        overLay.classList.add('overlay-slide-left');
+    }
+}
+
+// Event Listeners
+menuBars.addEventListener('click', toggleNav);
+nav1.addEventListener('click', toggleNav);
+nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
+nav4.addEventListener('click', toggleNav);
+nav5.addEventListener('click', toggleNav);
